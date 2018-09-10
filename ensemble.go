4fe478c76c9aefa5a175ec5105288f0911da9300
Kirk Scheibelhut <kjs@scheibo.com>
2018-09-09 19:32:03 -0700

    Give up on ensemble model

--- ensemble.go
+++ ensemble.go
@@ -69,6 +69,7 @@ func weightIfUnset(key string, weight *float64, val *float64) {
 		*weight = val
 	}
 }
+
 type providerWeight struct {
 	provider provider
 	weight float64
@@ -99,14 +100,160 @@ func newEnsembleProvider(o *options) (*provider, error) {
 	return providers, nil
 }
 
-func (w *ensembleProvider) current(ll geo.LatLng) (*Conditions, error) {
-	return w.providers[0].current(ll) // TODO
+type ensembleProvider {
+  providers []*providerWeight
 }
 
-func (w *ensembleProvider) forecast(ll geo.LatLng) (*Forecast, error) {
-	return w.providers[0].forecast(ll) // TODO
+func configured(kw keyWeight) bool {
+	return kw.key != "" && kw.weight != 0
+}
+
+func newEnsembleProvider(o *options) (*provider, error) {
+	var providers []*providerWeights
+	if configured(o.ds) {
+		p := &providerWeights{
+			provider: newDarkSkyProvider(options.ds.key),
+			weight: options.ds.weight,
+		}
+	}
+	// TODO(kjs): add other providers
+
+	if len(providers) == 0 {
+		return nil, fmt.Errorf("not configured with any weather providers")
+	}
+
+	return providers, nil
+}
+
+type conditionsWeight struct {
+	conditions *Conditions
+	weight float64
+}
+
+func (w *ensembleProvider) current(ll geo.LatLng) (*Conditions, error) {
+	return get(ll, time.Now(), func(p *provider, ll geo.LatLng, t time.Time) (*Conditions, error){
+		return p.current(ll)
+	})
 }
 
 func (w *ensembleProvider) history(ll geo.LatLng, t time.Time) (*Conditions, error) {
-	return w.providers[0].history(ll) // TODO
+	return get(ll, time.Now(), func(p *provider, ll geo.LatLng, t time.Time) (*Conditions, error){
+		return p.history(ll, t)
+	})
+}
+
+func (w *ensembleProvider) get(ll geo.LatLng, t time.Time, call func(*provider, geo.LatLng, time.Time)(*Condition, error)) (*Condition, error) {
+  conds := make(chan *conditionWeight, len(w))
+	errs := make(chan error, len(w))
+
+	for _, pw := range w.providers {
+		go func(p provider) {
+			c, err := call(p, ll, t)
+			if err != nil {
+				errs <- err
+				return
+			}
+			conds <- &conditionWeight{condition: c, weight: pw.weight}
+		}(pw.provider)
+	}
+
+	var conditions []*conditionWeight
+	var errors []error
+
+	for i := 0; i < len(w.providers); i++ {
+		select {
+		case cond := <-conds:
+			conditions = append(conditions, cond)
+		case err := <-errs:
+			errors = append(errors, err)
+		}
+	}
+
+	if len(conditions) > 0 {
+		return w.weightCondition(conditions), nil
+	}
+	return nil, fmt.Errorf("errors: %+v", errors)
+}
+
+type forecastWeight struct {
+	forecast *Forecast
+	weight float64
+}
+
+func (w *ensembleProvider) forecast(ll geo.LatLng) (*Forecast, error) {
+  fores := make(chan *forecastWeight, len(w))
+	errs := make(chan error, len(w))
+
+	for _, pw := range w.providers {
+		go func(p provider) {
+			f, err := p.forecast(ll)
+			if err != nil {
+				errs <- err
+				return
+			}
+			fors <- &forecastWeight{forecast: f, weight: pw.weight}
+		}(pw.provider)
+	}
+
+	var forecasts []*forecastWeight
+	var errors []error
+
+	for i := 0; i < len(w.providers); i++ {
+		select {
+		case fore := <-fores:
+			forecasts = append(forecasts, fores)
+		case err := <-errs:
+			errors = append(errors, err)
+		}
+	}
+
+	if len(forecasts) > 0 {
+		return w.weightForecasts(forecasts), nil
+	}
+	return nil, fmt.Errorf("errors: %+v", errors)
+}
+
+func (w *ensembleProvider) weightConditions(conditions []*conditionWeight) (*Conditions, error) {
+	c := Conditions{}
+
+	weights := 0.0
+	for _, cw := range conditions {
+		weights += cw.weight
+	}
+
+	for _, cw := range conditions {
+		w := cw.weight/weights
+		c.Temperature += cw.conditions.Temperature * w
+		c.Humidity += cw.conditions.Humidity * w
+		c.PrecipProbability += cw.conditions.PrecipProbability * w
+		c.PrecipIntensity += cw.conditions.PrecipIntensity * w
+		c.AirPressure += cw.conditions.AirPressure * w
+		c.AirDensity += cw.conditions.AirDensity * w
+		c.WindSpeed += cw.conditions.WindSpeed * w
+		c.WindGust += cw.conditions.WindGust * w
+		c.WindBearing += cw.conditions.WindBearing * w // TODO average angles...
+		c.UVIndex += cw.conditions.UVIndex * w
+	}
+}
+
+func (w *ensembleProvider) weightForecast(forecasts []*forecastWeight) (*Forecast, error) {
+	var hourlies [][]*Conditions
+	var dailies [][]*Conditions
+
+	for _, forecast := range forecasts {
+		hourlies = append(hourlies, forecast.Hourly)
+		dailies = append(dailies, forecast.Daily)
+	}
+
+	// TODO
+	var hourly []*Condition
+	for i = 0; i < len(hourlies); i++ {
+		var conditions []*conditionWeights
+		for j = 0; i < len(hourlies[i]); j++ { //hour
+			con
+		}
+	}
+
+	for _, hourly := range hourlies {
+	}
 }
